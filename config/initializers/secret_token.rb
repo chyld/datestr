3c3d330457bc168ba48b791ec13be9ee22a689ab
Chyld Medford <chyld.medford@gmail.com>
2012-11-20 14:23:22 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datestr::Application.config.secret_token = '0bcb386a5c2fb015bcfcee6e9422d64c70c9c353b68cfdb2e75f3b38fd2811c61af5a4f6e69fb40ece6b69e843595970063f272b747dbb9f23e1ead121537d31'
