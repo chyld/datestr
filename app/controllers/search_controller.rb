fe2f05a9b4a6ada9605c254e2ca6be1a8fe5db55
Chyld Medford <chyld.medford@gmail.com>
2012-11-26 11:18:25 -0500

    work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
     search = params[:search]
     @profiles = User.where(nil)
     @profiles = @profiles.where(:age => search[:age]) if search[:age].present?
-    @profiles = @profiles.where(:age => search[:gender]) if search[:gender].present?
-    @profiles = @profiles.where(:age => search[:state]) if search[:state].present?
-    @profiles = @profiles.where(:age => search[:ethnicity]) if search[:ethnicity].present?
+    @profiles = @profiles.where(:gender => search[:gender]) if search[:gender].present?
+    @profiles = @profiles.where(:state => search[:state]) if search[:state].present?
+    @profiles = @profiles.where(:ethnicity => search[:ethnicity]) if search[:ethnicity].present?
     render 'users/index'
   end
 end
